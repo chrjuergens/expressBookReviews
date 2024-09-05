f366d326c44f36f1b432dd33353481f169e3bdbc
christian <cjuergens@mailbox.org>
2024-09-05 09:42:21 +0000

    Task 5

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -56,8 +56,11 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+  return res.send(JSON.stringify(book["reviews"]));
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //return res.status(300).json({message: "Yet to be implemented"});
 });
 
 module.exports.general = public_users;
