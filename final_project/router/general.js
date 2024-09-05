b274c06522cca8da489532624ad79597880e406c
christian <cjuergens@mailbox.org>
2024-09-05 08:43:31 +0000

    Task 2

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -19,8 +19,10 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
+  const isbn = req.params.isbn;
+  return res.send(JSON.stringify(books[isbn]));
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //return res.status(300).json({message: "Yet to be implemented"});
  });
   
 // Get book details based on author
