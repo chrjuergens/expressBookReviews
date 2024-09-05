764a4bb340ba24b875b3d82b76b2fb033d4c0d74
christian <cjuergens@mailbox.org>
2024-09-05 11:00:37 +0000

    Task 6

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -6,11 +6,35 @@ const regd_users = express.Router();
 let users = [];
 
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+  if (username) {
+    // Filter the users array for any user with the same username
+    let userswithsamename = users.filter((user) => {
+      return user.username === username;
+    });
+    // Return true if any user with the same username is found, otherwise false
+    if (userswithsamename.length > 0) {
+      return false;
+    } else {
+      return true;
+    }
+  } else {
+    false;
+  }
+  //write code to check is the username is valid
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+  // Filter the users array for any user with the same username and password
+  let validusers = users.filter((user) => {
+    return (user.username === username && user.password === password);
+  });
+  // Return true if any valid user is found, otherwise false
+  if (validusers.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
+  //write code to check if username and password match the one we have in records.
 }
 
 //only registered users can login
